80acc0304de480a99955fa8405310dfaa6804ec8
mohammedghoula2 <mohammedghoula2@gmail.com>
2025-01-17 15:40:54 +0200

    Finalizing the login auth

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import City from './components/City';
 import Form from './components/Form';
 import { CitiesProvider } from './Contexts/CitiesContext.jsx';
 import { AuthProvider } from './Contexts/FakeAuthContext.jsx';
+import ProtectedRoute from './pages/ProtectedRoute.jsx';
 
 function App() {
   return (
@@ -24,7 +25,14 @@ function App() {
             <Route path='product' element={<Product />} />
             <Route path='pricing' element={<Pricing />} />
             <Route path='login' element={<Login />} />
-            <Route path='app' element={<AppLayout />}>
+            <Route
+              path='app'
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate to='cities' replace />} />
               <Route path='cities/:id' element={<City />} />
               <Route path='cities' element={<CityList />} />
